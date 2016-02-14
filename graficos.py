527a55355ba15fa91590d2e46de9eca40a8787b5
Andres <andres3207@gmail.com>
2016-02-14 11:03:03 -0300

    agregado el server socket TCP

--- graficos.py
+++ graficos.py
@@ -1,45 +1,74 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtNetwork import *
+#from PyQt5.QtGlobal import *
+#import socket
 
 
 class Ventana(QWidget):
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+    def __init__(self, *args): 
+        QWidget.__init__(self, *args)
 
-    def initUI(self):
         self.setGeometry(0,0,1366, 768)
         self.setWindowTitle('Titulo de ventana')
         label = QLabel('Zona de dibujo')
         layout = QVBoxLayout()
         layout.addWidget(label)
-        #layout.setAlignment(Qt.AlignCenter)
+
         self.vista=QGraphicsView()
         self.vista.setFixedSize(1440,900)
         self.escena=QGraphicsScene()
         self.vista.setScene(self.escena)
         layout.addWidget(self.vista)
         self.setLayout(layout)
-        zzz=self.vista.geometry;
-        #print(type(zzz))
-        #print("X: "+zzz+", Y: "+zzz)
-        self.dibujar()
 
-    def dibujar(self):
-    	self.escena.addLine(-683,-384,683,384)
-    	self.escena.addLine(-683,384,683,-384)
-    	zzz=self.vista.geometry;
-    	#print("X: "+zzz+", Y: "+zzz)
+        self.timer1=QTimer()
+        print("inicio socket")
+        self.socket=QTcpSocket()
+        self.server=QTcpServer()
+        self.server.listen(QHostAddress.Any,1234)
+        print("Listening")
+        self.server.newConnection.connect(self.aceptarConexion)
+        self.timer1.timeout.connect(self.timer1Timeout)
+
+
+        self.dibujar(3,2)
+
+
+    def dibujar(self,x,y):
+    	self.escena.addLine(x,y,x,y)
+    	#self.escena.addLine(10,10,10,10)
+    def aceptarConexion(self):
+    	print("Se ha conectado un cliente")
+    	self.socket=self.server.nextPendingConnection()
+    	self.timer1.setInterval(1000)
+    	self.timer1.start()
+    def timer1Timeout(self):
+    	print("timeout")
+    	datos=self.socket.readAll()
+    	n=datos.size()
+    	i=0
+    	while i<n:
+    		x=datos.at(i)
+    		y=datos.at(i+1)
+    		i=i+2
+    		print("X: "+x+" Y: "+y)
+    		self.dibujar(int(x),int(y))
+    	#print(n)
+
+    def timerEvent(self,QTimerEvent):
+    	print("Timer")
+
+
+
 
 
 if __name__ == '__main__':
-
-    import sys
-    from PyQt5.QtWidgets import QApplication
 
     app = QApplication(sys.argv)
     ex = Ventana()
